10c17b507aa9294285605569ad859d23b62f61f7
Eric Yi <erickanginyi@gmail.com>
2021-09-27 13:03:29 -0400

    commit tree 75705f3e6e12a38b52900f87627bdbdcb263f6e2

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [toCart, setToCart] = useState(false);
+
+  function asdf () {
+    setToCart(toCart => !toCart);
+  }
+
+  const addOrRemove = toCart ? "in-cart" : "";
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={asdf} className={addOrRemove}>{addOrRemove ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
